3aaa5b06479c27f16da30dd5d30b7fb499733b5d
kartelmode <65723695+kartelmode@users.noreply.github.com>
2020-12-29 17:22:27 +0300

    UpdTowerRotation

--- Assets/Scripts/Tank/TowerRotation.cs
+++ Assets/Scripts/Tank/TowerRotation.cs
@@ -13,24 +13,31 @@ namespace simpleproject.Tank.Tower.Rotation
             return (PositionY < 0.0);
         }
 
-        private void Update()
+        private void SeeOnMouse()
         {
+            double HalfCircle = 180.0;
+
             Vector3 MousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
             Vector3 MousePositionRelativelyTankPosition = MousePosition - _tankPosition.position;
 
             double alpha = (Mathf.PI / 2 - Mathf.Atan(MousePositionRelativelyTankPosition.x /
                                                       MousePositionRelativelyTankPosition.y)) *
-                            180.0 / Mathf.PI;
+                            HalfCircle / Mathf.PI;
 
             if (BelowTank(MousePositionRelativelyTankPosition.y))
-                alpha += 180.0;
+                alpha += HalfCircle;
 
             Vector3 NewRotationTower = new Vector3(_towerTransform.eulerAngles.x,
                                                    _towerTransform.eulerAngles.y,
                                                    (float)alpha - 90.0f);
 
             _towerTransform.eulerAngles = NewRotationTower;
+        }
+
+        private void Update()
+        {
+            SeeOnMouse();
         }
     }
 }
